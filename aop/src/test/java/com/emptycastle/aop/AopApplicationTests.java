44742ac9694c6394f483f6ae6bc4d5bdb811ade0
wesbin <leegosoo11@naver.com>
2022-01-03 09:17:48 +0900

    AOP start

---
+++ aop/src/test/java/com/emptycastle/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emptycastle.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
