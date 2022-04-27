1b13381022c86dfdd57424641e11934ca2c12554
wesbin <leegosoo11@naver.com>
2022-04-27 11:55:09 +0900

    composable

---
+++ composable/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {useMouse} from '@/composables/mouse';
+
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <div>
+    Mouse position is at: {{ x }}, {{ y }}
+  </div>
+</template>
---
+++ composable/src/composables/mouse.js
@@ -0,0 +1,19 @@
+import {ref} from 'vue';
+import {useEventListener} from '@/composables/event';
+
+export function useMouse() {
+  const x = ref(0)
+  const y = ref(0)
+
+  function update(event) {
+    x.value = event.pageX
+    y.value = event.pageY
+  }
+
+  useEventListener(window, 'mousemove', update)
+
+  // onMounted(() => window.addEventListener('mousemove', update))
+  // onUnmounted(() => window.addEventListener('mousemove', update))
+
+  return { x, y }
+}
