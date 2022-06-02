48be0bab360804123259e0680aa1924bf478f75b
wesbin <leegosoo11@naver.com>
2022-06-02 11:24:42 +0900

    ToArrayWithCast.kt update #2

--- Kotlin/src/ToArrayWithCast.kt
+++ Kotlin/src/ToArrayWithCast.kt
@@ -1,4 +1,4 @@
 fun main () {
-    val anyList: List<Any> = listOf(0, 1, 2, 3)
-    val strArray: Array<String> = anyList.map { it as String }.toTypedArray()
+    val anyList: List<Number> = listOf(1, 2, 3)
+    val wantArray: Array<Int> = anyList.map { it as Int }.toTypedArray()
 }
