cd9605ff1763a01cfe00a57596bccb236c1a6749
wesbin <leegosoo11@naver.com>
2022-07-04 15:12:47 +0900

    SonarLint check update

--- DesignPatterns/factory-pattern/src/abstractFactory/computerPart/cpu/CostPerformanceCPU.kt
+++ DesignPatterns/factory-pattern/src/abstractFactory/computerPart/cpu/CostPerformanceCPU.kt
@@ -1,5 +1,3 @@
 package abstractFactory.computerPart.cpu
 
-import abstractFactory.computerPart.ComputerPart
-
 class CostPerformanceCPU: CPU()
--- DesignPatterns/factory-pattern/src/abstractFactory/computerPart/storage/HDD.kt
+++ DesignPatterns/factory-pattern/src/abstractFactory/computerPart/storage/HDD.kt
@@ -1,5 +1,3 @@
 package abstractFactory.computerPart.storage
 
-import abstractFactory.computerPart.ComputerPart
-
 class HDD: Storage()
